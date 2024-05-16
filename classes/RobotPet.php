71c1cd1685eb9965abfce0faadf02f73a847d4c1
dpjprogramming <cheesyd64@hotmail.com>
2024-05-16 14:26:09 -0400

    modified index and pet class getters and setters

--- classes/RobotPet.php
+++ classes/RobotPet.php
@@ -3,4 +3,16 @@
 class RobotPet extends Pet
 {
     private $accessories = array();
+
+    public function getAccessories(): array
+    {
+        return $this->accessories;
+    }
+
+    public function setAccessories(array $accessories): void
+    {
+        $this->accessories = $accessories;
+    }
+
+
 }
--- classes/StuffedPet.php
+++ classes/StuffedPet.php
@@ -2,8 +2,46 @@
 
 class StuffedPet extends Pet
 {
-    private $size;
-    private $stuffingType;
-    private $material;
+    private $_size;
+    private $_stuffingType;
+    private $_material;
+
+
+    function setSize($size){
+        $this->_size = $size;
+    }
+    function setStuffingType($stuffing){
+        $this->_stuffingType = $stuffing;
+    }
+
+    function setMaterial($material){
+        $this->_material = $material;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->_size;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStuffingType()
+    {
+        return $this->_stuffingType;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMaterial()
+    {
+        return $this->_material;
+    }
+
+
 
 }
